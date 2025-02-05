b6acbc9eaa731ce4054c711896f50d1c15f0200a
GreedyYB <b_l_kidd@hotmail.com>
2025-02-04 18:01:11 -0800

    Moved index.html to public folder and updated root route

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const config = {
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 // Start server
 const PORT = process.env.PORT || 3000;
